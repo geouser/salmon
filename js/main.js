f85770f272809c0930d52121419f7a0ce74aed4e
Serhiy <serhiyhulyi93@gmail.com>
2016-04-04 13:25:33 +0300

    almost ready

--- js/main.js
+++ js/main.js
@@ -40,6 +40,46 @@ jQuery(document).ready(function($) {
 
   $('#side_pannel').perfectScrollbar();
 
+  var map;
+  function googleMap_initialize() {
+    var lat = $('#map-canvas').attr('data-lat');
+    var long = $('#map-canvas').attr('data-long');
+    var mapCenterCoord = new google.maps.LatLng(lat, long-0.004);
+    if ($(window).width() > 1600 ) {
+      var mapCenterCoord = new google.maps.LatLng(lat, long-0.004);
+    } else {
+      var mapCenterCoord = new google.maps.LatLng(lat, long-0.003);
+    }
+    var mapMarkerCoord = new google.maps.LatLng(lat, long);
+
+    var mapOptions = {
+      center: mapCenterCoord,
+      zoom: 17,
+      //draggable: false,
+      disableDefaultUI: true,
+      scrollwheel: false,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var markerImage = new google.maps.MarkerImage('images/marker.png');
+    var marker = new google.maps.Marker({
+      icon: markerImage,
+      position: mapMarkerCoord, 
+      map: map,
+      title:"ДИКИЙ ЛОСОСЬ"
+    });
+
+    var markersBounds = new google.maps.LatLngBounds();
+    console.log(markersBounds.extend(mapCenterCoord));
+    
+    $(window).resize(function (){
+      map.setCenter(mapCenterCoord);
+    });
+  };
+  googleMap_initialize();
+
+
 });
 
 
